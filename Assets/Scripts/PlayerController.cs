05411b2873924e75e71def7b55ef96d01abedf6c
jsams <jsams@fallingsloth.uk>
2018-07-22 15:50:35 +0100

    Started on setting up the player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player collided with " + collision.gameObject.name);
+    }
+}
