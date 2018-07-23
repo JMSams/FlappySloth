3a4a1e6eb8676fb2e02fa89a26c44c19a89dbc80
jsams <jsams@fallingsloth.uk>
2018-07-23 19:02:10 +0100

    .

---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    public Transform wallPrefab;
+
+    public float speed = 1f;
+
+    public float gapHalfSize = 2.5f;
+
+    Transform topPart, bottomPart;
+
+    void Start()
+    {
+        topPart = Instantiate(wallPrefab, this.transform);
+        topPart.localPosition = new Vector2();
+    }
+
+    void LateUpdate()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
